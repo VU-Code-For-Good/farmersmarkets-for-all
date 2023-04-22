9b9e2bb4b3b809717c9919ac30d93f4c12d1d11f
jazzyb.21 <jazzyb.207@gmail.com>
2023-04-22 09:23:53 -0500

    test: set the the test project up

---
+++ backend/FarmersMarketApi.Tests/FarmersMarketApi/Unit/GetFarmersMarketsByStateShould.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IO.Swagger.Controllers;
+
+namespace FarmersMarketApi.Tests;
+
+[TestClass, TestCategory("Unit")]
+public class GetFarmersMarketsByStateShould
+{
+    private FarmersMarketsApiController _sut;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _sut = new FarmersMarketsApiController();
+    }
+
+    [TestMethod]
+    public void Return500_WhenAnUnexpectedErrorOccurs()
+    {
+        Assert.Fail("");
+    }
+
+}
